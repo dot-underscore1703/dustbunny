122c2dcfac438ead9d6709721d5cd48db64b8b36
Dot Underscore <roryl12@outlook.com>
2026-08-25 13:12:20 +1000

    chore: bump version to 0.3.0 and add link to MIT license

--- src/headers/version.h
+++ src/headers/version.h
@@ -2,12 +2,13 @@
 #define VERSION_H
 
 #define DUSTBUNNY_VERSION_MAJOR 0
-#define DUSTBUNNY_VERSION_MINOR 2
+#define DUSTBUNNY_VERSION_MINOR 3
 #define DUSTBUNNY_VERSION_PATCH 0
 
 #define DUSTBUNNY_RELEASE_YEAR  2026
 #define DUSTBUNNY_AUTHOR        "Rory Lane"
 #define DUSTBUNNY_LICENSE       "MIT"
 #define DUSTBUNNY_REPO          "https://github.com/dot-underscore1703/dustbunny/"
-
+#define DUSTBUNNY_LICENSE_URL   "https://opensource.org/license/mit"
+ 
 #endif

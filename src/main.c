032bae62b0a1b7814d5262a74edd3c577356598e
Dot Underscore <roryl12@outlook.com>
2026-08-25 13:10:00 +1000

    feat: added --version arg and free input and token buffer

--- src/main.c
+++ src/main.c
@@ -1,20 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include "headers/version.h"
 #include "headers/command.h"
 
-int main() {
-    char *input;
-    while(1) {
-        input = readline(": ");
-        char **tokens = tokenise(input);
-        add_history(input);
-        if(execute(tokens) == -1){
-            break;
+int main(int argc, char **argv) {
+    if(argc < 2){
+        char *input;
+        while(1) {
+            input = readline(": ");
+            add_history(input);
+            char **tokens = tokenise(input);
+            if(execute(tokens) == -1){
+                break;
+            }
+            free(input);
+            free(tokens);
+        }
+
+        fprintf(stderr,"dustbunny: goodbye!\n");
+    } else {
+        if(strcmp(argv[1],"--version") == 0){
+            printf(
+                "Dustbunny, version %i.%i.%i\n"
+                "Copyright (c) %i %s.\n"
+                "License %s\n\n"
+                "Read the full license at %s\n"
+                "You can view the Dustbunny repo at %s\n",
+                DUSTBUNNY_VERSION_MAJOR,
+                DUSTBUNNY_VERSION_MINOR,
+                DUSTBUNNY_VERSION_PATCH,
+                DUSTBUNNY_RELEASE_YEAR,
+                DUSTBUNNY_AUTHOR,
+                DUSTBUNNY_LICENSE,
+                DUSTBUNNY_LICENSE_URL,
+                DUSTBUNNY_REPO
+            );
         }
     }
-
-    //free(buffer_ptr);
-    fprintf(stderr,"dustbunny: goodbye!\n");
     return 0;
 }
